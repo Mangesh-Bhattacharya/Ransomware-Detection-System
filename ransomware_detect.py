ef28040f9e80758680918a163dd26d033ec0960b
TheCyberRainbow <84471637+Mangesh-Bhattacharya@users.noreply.github.com>
2025-02-08 15:29:14 -0500

    commit

--- ransomware_detect.py
+++ ransomware_detect.py
@@ -1,1 +1,9 @@
 import os
+import psutil
+import time
+import tkinter as tk
+from tkinter import messagebox
+from watchdog import Obeserver
+from watchdog.events import FileSystemEventHandler
+
+# Suspicious file extensions often used by ransomware
