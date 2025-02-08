f213365294100a458e276e1e374ed167eb9785ff
TheCyberRainbow <84471637+Mangesh-Bhattacharya@users.noreply.github.com>
2025-02-08 16:03:24 -0500

    commit

--- ransomware_detect.py
+++ ransomware_detect.py
@@ -23,3 +23,34 @@ class RansomwareMonitor(FileSystemEventHandler):
     def block_ransomware():
         """ Kill suspicious processes """
         for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                if any(word in proc.info['name'].lower() for word in ["ransom", "encrypt", "locker"]):
+                    psutil.Process(proc.info['pid']).terminate()
+                    print(f"Blocked process: {proc.info['name']}")
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+            
+    def start_monitoring(alert_function):
+        """ Start monitoring for suspicious files """
+        Obeserver = Obeserver()
+        event_handler = RansomwareMonitor(alert_function)
+        Obeserver.schedule(event_handler, path=os.path.expanduser("~"), recursive=True)
+        Obeserver.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            Obeserver.stop()
+        Obeserver.join()
+        
+    def start_gui():
+        """ Ransomware Detector Application """
+        root = tk.Tk()
+        root.title("Ransomware Detector System")
+        root.geometry("400x300")
+        label.pack(pady=20)
+        start.monitoring(alert_user)
+        root.mainloop
+        
+    if __name__ == "__main__":
+        start_gui()
